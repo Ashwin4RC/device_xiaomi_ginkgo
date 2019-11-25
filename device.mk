8bffe6411ea92246b53a85f3f9234f0660f54beb
Ashwin R C <ashwin2001achu@gmail.com>
2019-11-25 15:17:31 +0000

    ginkgo: build audio.primary.trinket from source

--- device.mk
+++ device.mk
@@ -75,6 +75,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.1-impl:32 \
     audio.a2dp.default \
+    audio.primary.trinket \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
